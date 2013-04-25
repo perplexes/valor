577598dafcefc73606913124f449258f46bd9b3f
Colin Curtin <colin.t.curtin@gmail.com>
2013-04-25 03:08:08 -0700

    There's stars. Sort of. That go away randomly.

--- main.elm
+++ main.elm
@@ -31,8 +31,8 @@ clamp min max x =
 --foreign export jsevent "elm_log"
 --  log :: Signal JSString
 
-viewW = 640
-viewH = 480
+mapW = 6400
+mapH = 4800
 shipW = 170 / 4
 shipH = 166 / 4
 
@@ -49,8 +49,8 @@ defaultGame = GameState { x=100.0, y=100.0, angle=0.0, dx=0.0, dy=0.0 }
 
 stepGame (Input t (UserInput ui)) (GameState gs) =
   let {x,y,angle,dx,dy} = gs in
-  GameState { gs | dx <- clamp (0-100) 100 (dx + toFloat ui.y * sin angle)
-                 , dy <- clamp (0-100) 100 (dy + toFloat ui.y * cos angle * (0-1))
+  GameState { gs | dx <- clamp (0-100) 100 (dx + toFloat ui.y * 2 * sin angle)
+                 , dy <- clamp (0-100) 100 (dy + toFloat ui.y * 2 * cos angle * (0-1))
                  , angle <- angle + t * 3 * toFloat ui.x
                  , x <- clamp (shipW/2) (mapW - shipW/2) $ x + t * dx
                  , y <- clamp (shipH/2) (mapH - shipH/2) $ y + t * dy}
@@ -97,29 +97,73 @@ tile c r xAdj yAdj =
 
 starTilesize = 1024
 starDensity = 31
-randstar = JavaScript.randInRange 0 starTilesize
-startile = map (\_ (randstar, randstar)) [0..starDensity]
-starsLevel1 = startile
-starsLevel2 = startile
+randstar _ = JavaScript.randInRange 0 starTilesize
+starPoints = map (\x -> (randstar x, randstar x)) [0..starDensity]
+starsLevel1 = starPoints -- closer
+starsLevel2 = starPoints -- farther
+l1color = rgb 184 184 184
+l2color = rgb 96 96 96
+
+starLayers (vx,vy,vw,vh) =
+  let left = floor $ (vx - vw) / (toFloat starTilesize)
+      top = floor $ (vy - vh) / (toFloat starTilesize)
+      right = floor $ (vx + vw) / (toFloat starTilesize)
+      bottom = floor $ (vy + vh) / (toFloat starTilesize)
+      st (c,r) = starTile (c,r) (vx,vy,vw,vh)
+  in foldl (\c a ->
+       foldl (\r a2 ->
+         a2 ++ st (c,r)
+       ) a [left..right]
+     ) [] [top..bottom]
+
+starTile (c,r) (vx,vy,vw,vh) =
+  let dx1 x = (c * starTilesize + x - vx + vw) / 2
+      dy1 y = (r * starTilesize + y - vy + vh) / 2
+      dx2 x = (c * starTilesize + x - vx) / 3 + vw/2
+      dy2 y = (r * starTilesize + y - vy) / 3 + vh/2
+      star color (x, y) = filled color (rect 1 1 (x,y))
+  in map (\(x,y) -> star l1color (dx1 x, dy1 y)) starsLevel1 ++
+     map (\(x,y) -> star l2color (dx2 x, dy2 y)) starsLevel2
 
 background w h = filled black (rect w h (w/2,h/2))
 --background w h = textured "hubble.jpg" (rect w h (0,0))
 --ship x y angle = rotate (0.08 + (angle / (2 * pi))) (filled black (ngon 3 10 (x,y)))
-ship angle = rotate (angle / (2 * pi)) $ sprite "ship2.png" shipW shipH (((viewW/2) - (shipW/2)), ((viewH/2) - (shipH/2)))
+ship vw vh angle =
+  rotate (angle / (2 * pi)) $
+    sprite "ship2.png" shipW shipH (vw/2 - shipW/2, vh/2 - shipH/2)
 
-display (w,h) (GameState gameState) =
-  --log $ show gameState
-  container w h topLeft $ collage (viewW) (viewH) [ background (viewW) (viewH), ship gameState.angle]
+viewPort w h x y =
+  let vx = x - (toFloat w)/2
+      vy = y - (toFloat h)/2
+  in (vx,vy,w,h)
+
+whiteTextForm string =
+  text . Text.color white $ toText string
+
+display (width,height) (GameState gameState) =
+  let sl = starLayers $ viewPort width height gameState.x gameState.y
+      w = floor width
+      h = floor height
+      layers = [ background w h ] ++ sl ++ [ ship w h gameState.angle ] ++ [whiteTextForm $ "hello"]
+  in container w h topLeft $ collage w h layers
+  --in container w h topLeft $ collage w h $ [whiteTextForm $ show st] ++ [background w h] ++ st
+  --in asText $ show (w,h,(viewPort width height gameState.x gameState.y),sl)
+  --in asText $ show (background w h)
     --  --, text . Text.color
     --]
     --asText $ show gameState,
     --asText $ show (w,h)
   --]
--- display (w,h) gameState = asText gameState
 
-delta = lift inSeconds (fps 60)
+--display (w,h) (GameState gameState) =
+--  let vx = gameState.x - (toFloat w)/2
+--      vy = gameState.y - (toFloat h)/2
+--  in show $ randstar
+
+delta = lift inSeconds (fps 30)
 input = sampleOn delta (lift2 Input delta userInput)
 
 gameState = foldp stepGame defaultGame input
 
 main = lift2 display Window.dimensions gameState
+--main = asText randstar
