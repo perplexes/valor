d014d07fd0f9c8645a6338a07974c5183db0dcba
Colin Curtin <colin.t.curtin@gmail.com>
2013-04-27 18:16:56 -0700

    Code cleanup and comments.

--- Subspace.elm
+++ Subspace.elm
@@ -3,8 +3,13 @@ import Keyboard
 import Window
 import Graphics.Text as T
 --import Starfield
-
 import Random
+
+-- Constants
+mapW = 6400
+mapH = 4800
+shipW = 170
+shipH = 166
 
 starTilesize = 1024
 starDensity = 31
@@ -14,13 +19,14 @@ starsLevel1 = starPoints -- closer
 starsLevel2 = starPoints -- farther
 l1color = rgb 184 184 184
 l2color = rgb 96 96 96
---(vw,vh,sx,sy)
+
+-- Convenience type for the width & height of the view port,
+-- as well as the ship's current (x,y) coordinates
 type ViewPort = (Float,Float,Float,Float)
 viewPort (vw,vh,sx,sy) = (toFloat vw, toFloat vh, sx, sy)
 
---starLayers (vw,iVh,sx,sy) =
---  map (\tile -> starTile tile(iVw,iVh,sx,sy,starTilesize)) tiles
-
+-- Draw star tiles to the viewport. + debug info
+-- My kingdom for an array comprehension.
 starLayers : ViewPort -> (Int,Int,Int,Int,[Int],[Int],[Form])
 starLayers vp =
   -- We pad by half a viewport width/height on each side
@@ -40,6 +46,9 @@ starLayers vp =
        ) a ltr
      ) [] ttb)
 
+-- Draw a particular tile, which is actually two star tiles:
+-- Level 1 stars are closer and a bit brighter, and move at /2 speed.
+-- Level 2 stars are further, a bit darker, and move even slower.
 starTile : ViewPort -> (Int, Int) -> [Form]
 starTile (vw,vh,sx,sy) (c,r)  =
   let absX x = c * starTilesize + x
@@ -61,15 +70,7 @@ clamp min max x =
      | x < max -> x
      | otherwise -> max
 
---log message = castStringToJSString message
---foreign export jsevent "elm_log"
---  log :: Signal JSString
-
-mapW = 6400
-mapH = 4800
-shipW = 170
-shipH = 166
-
+-- Input is currently Keyboard.arrows
 data UserInput = UserInput { x : Int, y : Int }
 
 userInput =
@@ -81,6 +82,8 @@ data GameState = GameState { x : Float, y : Float, angle : Float, dx : Float, dy
 
 defaultGame = GameState { x=100.0, y=100.0, angle=0.0, dx=0.0, dy=0.0, t=0.0 }
 
+-- Calculate new gamestate
+-- Old GameState + Input = New GameState
 stepGame (Input t (UserInput ui)) (GameState gs) =
   let {x,y,angle,dx,dy} = gs in
   GameState { gs | dx <- clamp (0-1000) 1000 (dx + toFloat ui.y * 10 * sin angle)
@@ -89,6 +92,7 @@ stepGame (Input t (UserInput ui)) (GameState gs) =
                  , x <- {-clamp (shipW/2) (mapW - shipW/2) <|-} x + t * dx
                  , y <- {-clamp (shipH/2) (mapH - shipH/2) <|-} y + t * dy
                  , t <- t}
+
 
 {- Display -}
 
@@ -108,9 +112,7 @@ display : (Int,Int) -> GameState -> Element
 display (w,h) (GameState gameState) =
   let vp = viewPort (w,h,gameState.x,gameState.y)
       (left,top,right,bottom,ltr,ttb,sl) = starLayers vp
-      --displayLayers = [ background w h] ++ sl ++ [ ship w h gameState.angle ]
       displayLayers = [ background w h] ++ sl ++ [ ship w h gameState.angle ]
-      --displayLayers = [ ship w h gameState.angle ]
   in container w h topLeft <| layers [
     collage w h displayLayers
     , flow down [
@@ -119,18 +121,11 @@ display (w,h) (GameState gameState) =
       --,debug "Startile" (starTile (w,h,gameState.x,gameState.y))
     ]
   ]
-  --in asText <| show (displayLayers)
 
 delta = lift inSeconds (fps 30)
-avgFPS = average 10 delta
+--avgFPS = average 10 delta
 input = sampleOn delta (lift2 Input delta userInput)
 
 gameState = foldp stepGame defaultGame input
 
 main = lift2 display dimensions gameState
---main = lift2 display (constant (640, 480)) gameState
-
---display (w,h) =
-  --asText <| show (w,h)
-  --container w h topLeft (collage w h [background w h])
---main = lift display dimensions
